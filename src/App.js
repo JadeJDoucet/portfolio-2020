0066b042eea4c6f78ead9f62533a96027f8ad043
jadejdoucet <jadejdoucet@gmail.com>
2020-08-13 21:07:28 -0500

    created base structure of website

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <div>
+          <h2>Jade Doucet</h2>
+          <h4>Software Engineer, Web Developer</h4>
+        </div>
+        <div className="icons-row">
+        <a href="https://linkedin.com/in/jadejdoucet">
+          <i className="fab fa-linkedin fa-2x"></i>
+        </a>
+        <a href="https://github.com/JadeJDoucet">
+          <i className="fab fa-github fa-2x"></i>
+        </a>
+        <a href="https://dev.to/jadejdoucet">
+          <i className="fab fa-dev fa-2x"></i>
+        </a>
+      </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
