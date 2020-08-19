62f834a8e45e385edf133d19df33e06347b4dd38
jadejdoucet <jadejdoucet@gmail.com>
2020-08-18 22:28:31 -0500

    updated structure of page

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -2,8 +2,14 @@ import React from 'react';
 // Maybe use material UI menu
 
 export default function Projects() {
+    // Maybe float image right to have text go around it
+    // If not, a div with flex-row
     return (
         <div id="project-container">
+            <div className="project">
+                <h2 className="project-title">Project Name</h2>
+                
+            </div>
 
         </div>
     );
