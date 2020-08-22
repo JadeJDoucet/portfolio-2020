15434d3373fa6f39db2353eaedd890758980510e
jadejdoucet <jadejdoucet@gmail.com>
2020-08-22 12:33:19 -0500

    added basic swiper page structure

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
 import React from 'react';
-import Swiper, { Navigation, Pagination } from 'swiper';
-Swiper.use([Navigation, Pagination]);
-
 import './App.css';
 import Home from './pages/Home';
 import About from './pages/About';
 import Projects from './pages/Projects';
 import Blogs from './pages/Blogs';
+import Swiper, { Navigation, Pagination } from 'swiper';
+Swiper.use([Navigation, Pagination]);
 
 function App() {
   return (
-    <div className="App-container">
-      <Home />
-      <Projects />
-      <About />
-      <Blogs />
+    <div className="App-container swiper-container">
+      <div className="swiper-wrapper">
+        <Home className="swiper-slide" />
+        <Projects className="swiper-slide" />
+        <About className="swiper-slide" />
+        <Blogs className="swiper-slide" />
+      </div>
     </div>
   );
 }
