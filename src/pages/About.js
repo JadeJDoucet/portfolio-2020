6085c51450f55ae12191770fa744cd52edd0d92f
jadejdoucet <jadejdoucet@gmail.com>
2020-08-26 22:37:07 -0500

    updated page structure

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,16 +3,18 @@ import React from 'react';
 export default function About() {
     return (
         <div id="about">
-            <h2 className="page-title">Bio</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Vestibulum sagittis nisl in odio imperdiet, pharetra malesuada ex accumsan.
-                Integer urna nulla, varius et ornare in, tincidunt non justo.
-                Quisque et orci vel lorem luctus vestibulum. Vivamus sed est sit amet metus commodo condimentum eu eget arcu.
-                Donec eget enim eget elit venenatis volutpat non iaculis dolor. Praesent pharetra in tortor porttitor molestie.
-                In vitae mi ac ex scelerisque pretium. Mauris vehicula tortor ac pretium tristique. In fringilla urna sed tempus tincidunt.
-                Vestibulum vel enim ullamcorper, lacinia ex non, maximus mi. Quisque vel metus a metus rhoncus vehicula.
-                Integer sagittis nunc mi, a commodo tellus finibus ac.
-            </p>
+            <div id="bio-container">
+                <h2 className="page-title">Bio</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Vestibulum sagittis nisl in odio imperdiet, pharetra malesuada ex accumsan.
+                    Integer urna nulla, varius et ornare in, tincidunt non justo.
+                    Quisque et orci vel lorem luctus vestibulum. Vivamus sed est sit amet metus commodo condimentum eu eget arcu.
+                    Donec eget enim eget elit venenatis volutpat non iaculis dolor. Praesent pharetra in tortor porttitor molestie.
+                    In vitae mi ac ex scelerisque pretium. Mauris vehicula tortor ac pretium tristique. In fringilla urna sed tempus tincidunt.
+                    Vestibulum vel enim ullamcorper, lacinia ex non, maximus mi. Quisque vel metus a metus rhoncus vehicula.
+                    Integer sagittis nunc mi, a commodo tellus finibus ac.
+                </p>
+            </div>
             <div id="job-container">
                 <div className="job">
                     <h3>Front-End</h3>
