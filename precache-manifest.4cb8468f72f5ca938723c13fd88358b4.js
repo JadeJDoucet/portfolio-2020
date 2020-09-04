a1bba180bcd5c4fac0397953193ef721096478a6
jadejdoucet <jadejdoucet@gmail.com>
2020-09-03 20:45:41 -0500

    Updates

---
+++ precache-manifest.4cb8468f72f5ca938723c13fd88358b4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfd5a2e2a98550f7ff84bd2c5d4df068",
+    "url": "/portfolio-2020/index.html"
+  },
+  {
+    "revision": "8d1afa7dfe0d0c53059f",
+    "url": "/portfolio-2020/static/css/2.68424154.chunk.css"
+  },
+  {
+    "revision": "37f2e0ac7fb0f477105b",
+    "url": "/portfolio-2020/static/css/main.ec53c235.chunk.css"
+  },
+  {
+    "revision": "8d1afa7dfe0d0c53059f",
+    "url": "/portfolio-2020/static/js/2.a4111a1e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio-2020/static/js/2.a4111a1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37f2e0ac7fb0f477105b",
+    "url": "/portfolio-2020/static/js/main.2e0729e5.chunk.js"
+  },
+  {
+    "revision": "bbf3efb9002603ffad2a",
+    "url": "/portfolio-2020/static/js/runtime-main.106eb575.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio-2020/static/media/logo.5d5d9eef.svg"
+  }
+]);
