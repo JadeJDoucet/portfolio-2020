56d9867662d703ccce1cd95f4a8932d8fff3094c
jadejdoucet <jadejdoucet@gmail.com>
2020-09-03 21:40:13 -0500

    updated project URLs

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -12,13 +12,13 @@ export default function Projects() {
                 
                 </div>
             </a>
-            <a className="project-link" href="https://github.com/Assert-Reconceptualization/ScannAr" target="_blank" rel="noopener noreferrer">
+            <a className="project-link" href="https://github.com/JadeJDoucet/adopt-a-pothole" target="_blank" rel="noopener noreferrer">
                 <div className="project">
                 <h2 className="project-title">Adopt-A-Pothole</h2>
                 
                 </div>
             </a>
-            <a className="project-link" href="https://github.com/Assert-Reconceptualization/ScannAr" target="_blank" rel="noopener noreferrer">
+            <a className="project-link" href="https://github.com/Adopt-A-Pothole/Readr" target="_blank" rel="noopener noreferrer">
                 <div className="project">
                 <h2 className="project-title">Readr</h2>
                 
