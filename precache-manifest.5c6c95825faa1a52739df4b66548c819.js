d55e965380db6a703a77f47bc8a6b2d2616366cd
jadejdoucet <jadejdoucet@gmail.com>
2020-09-05 22:12:03 -0500

    Updates

---
+++ precache-manifest.5c6c95825faa1a52739df4b66548c819.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff90980b2a73e8b92849c4623947899c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "964e132097ba653a6c9c",
+    "url": "/static/css/2.68424154.chunk.css"
+  },
+  {
+    "revision": "d95c0d2040c1bef37cb8",
+    "url": "/static/css/main.df183aa6.chunk.css"
+  },
+  {
+    "revision": "964e132097ba653a6c9c",
+    "url": "/static/js/2.ee9a8102.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ee9a8102.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d95c0d2040c1bef37cb8",
+    "url": "/static/js/main.077a8eb4.chunk.js"
+  },
+  {
+    "revision": "55cf03ad92c385910fbc",
+    "url": "/static/js/runtime-main.e40405f4.js"
+  },
+  {
+    "revision": "3401514e05b7b35a5d88b8e242e644a1",
+    "url": "/static/media/jade_bg.3401514e.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1be2e30057136d7573265a572fc1c478.js"
+  "/precache-manifest.5c6c95825faa1a52739df4b66548c819.js"
 );
 
 self.addEventListener('message', (event) => {
