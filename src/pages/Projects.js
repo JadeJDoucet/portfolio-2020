3bb683e6a6a80c7d78826f9a089def9e745290f7
jadejdoucet <jadejdoucet@gmail.com>
2020-09-05 21:32:54 -0500

    updated structure

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -6,20 +6,20 @@ export default function Projects() {
     // If not, a div with flex-row
     return (
         <div id="projects">
-            <a className="project-link" href="https://github.com/Assert-Reconceptualization/ScannAr" target="_blank" rel="noopener noreferrer">
-                <div className="project project1">
+            <a className="project-link project1" href="https://github.com/Assert-Reconceptualization/ScannAr" target="_blank" rel="noopener noreferrer">
+                <div className="project">
                 <h2 className="project-title">ScannAR</h2>
                 
                 </div>
             </a>
-            <a className="project-link" href="https://github.com/JadeJDoucet/adopt-a-pothole" target="_blank" rel="noopener noreferrer">
-                <div className="project project2">
+            <a className="project-link project2" href="https://github.com/JadeJDoucet/adopt-a-pothole" target="_blank" rel="noopener noreferrer">
+                <div className="project">
                 <h2 className="project-title">Adopt-A-Pothole</h2>
                 
                 </div>
             </a>
-            <a className="project-link" href="https://github.com/Adopt-A-Pothole/Readr" target="_blank" rel="noopener noreferrer">
-                <div className="project project3">
+            <a className="project-link  project3" href="https://github.com/Adopt-A-Pothole/Readr" target="_blank" rel="noopener noreferrer">
+                <div className="project">
                 <h2 className="project-title">Readr</h2>
                 
                 </div>
